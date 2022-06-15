dbed6c1593a35790a2eb7499f06f6bd0ee7b70fc
Pedro Vicente Santos <pedrovicente.dev@gmail.com>
2022-06-15 20:16:29 -0300

    feat: add cache to routes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const apiCache = require('apicache');
 
 dotenv = require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
+const cache = apiCache.middleware;
+
 const app = express();
 
 app.use(cors());
 
-app.use('/api/v1', require('./routes'));
+app.use('/api/v1', cache('5 minutes'), require('./routes'));
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
